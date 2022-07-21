4a9d2d4b95b1a5c90fcfec61847564a864c496b5
sandman <sandwhen@gmail.com>
2022-07-22 00:37:11 +0700

    heer

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -21,13 +21,12 @@ const PostWidget = ({categories, slug}) => {
     }
   }, [slug]);
 
-  console.log(relatedPosts)
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
-      {relatedPosts.map((post) => (
-        <div className="flex items-center w-full mb-4">
+      {relatedPosts.map((post, index) => (
+        <div className="flex items-center w-full mb-4" key={index}>
           <div className='w-16 flex-none'>
             <img
               alt={post.title}
